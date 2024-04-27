6479c96a66e0a6434dde10d125b9053be8b4c23a
Daniel Kobiałka <daniel.kobialka.stud@pw.edu.pl>
2024-04-27 20:05:58 +0200

    APSI-12 added TicketService tests

---
+++ backend/src/test/java/apsi/team3/backend/service/TicketServiceTest.java
@@ -0,0 +1,43 @@
+package apsi.team3.backend.service;
+
+import apsi.team3.backend.DTOs.DTOMapper;
+import apsi.team3.backend.DTOs.TicketDTO;
+import apsi.team3.backend.model.Ticket;
+import apsi.team3.backend.repository.TicketRepository;
+import apsi.team3.backend.services.TicketService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class TicketServiceTest {
+
+    @InjectMocks
+    TicketService ticketService;
+
+    @Mock
+    TicketRepository ticketRepository;
+
+    @Test
+    public void testGetTicketById() {
+        Long ticketId = 1L;
+        TicketDTO ticketDTO = new TicketDTO(ticketId, null, null, null);
+        when(ticketRepository.findById(ticketId)).thenReturn(Optional.of(DTOMapper.toEntity(ticketDTO)));
+        assertEquals(ticketService.getTicketById(ticketId), Optional.of(ticketDTO));
+    }
+
+    @Test
+    public void testCreate() {
+        TicketDTO ticketDTO = new TicketDTO(1L, 1L, 1L, LocalDate.of(2024, 4, 27));
+        Ticket ticket = DTOMapper.toEntity(ticketDTO);
+        when(ticketRepository.save(any())).thenReturn(ticket);
+        assertEquals(ticketService.create(ticketDTO), ticketDTO);
+    }
+}
