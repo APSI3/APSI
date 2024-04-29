ea4f1f8e75b0e74077238b2a54b968ce6c00c181
Daniel Kobiałka <daniel.kobialka.stud@pw.edu.pl>
2024-04-29 20:29:45 +0200

    APSI-12 fixed bug in migration script

--- backend/src/main/resources/db/migration/V5__sample_event_ticket_data.sql
+++ backend/src/main/resources/db/migration/V5__sample_event_ticket_data.sql
@@ -1,6 +1,6 @@
 INSERT INTO events(name, start_date, end_date, description, organizer_id)
-VALUES ('testowe wydarzenie1', '2024-04-18 15:00:00', '2024-04-18 22:00:00', 'testowe wydarzenie dodane w migracji', 3),
-('testowe wydarzenie2', '2024-05-18 15:00:00', '2024-05-18 22:00:00', 'testowe wydarzenie dodane w migracji', 3);
+VALUES ('testowe wydarzenie1', '2024-04-18', '2024-04-18', 'testowe wydarzenie dodane w migracji', 3),
+('testowe wydarzenie2', '2024-05-18', '2024-05-18', 'testowe wydarzenie dodane w migracji', 3);
 
 INSERT INTO ticket_types(event_id, name, price, quantity_available)
 VALUES (1, 'tanie bilety', 9.99, 1000),
