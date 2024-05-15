8d45ae00dbba493e3e4080f0339105d2e3a45fc5
julia <julia.jodczyk@gmail.com>
2024-05-15 21:27:36 +0200

    APSI-25: fixed mail sending

---
+++ backend/src/main/java/apsi/team3/backend/model/MailStructure.java
@@ -0,0 +1,11 @@
+package apsi.team3.backend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MailStructure {
+    private String subject;
+    private String message;
+}
