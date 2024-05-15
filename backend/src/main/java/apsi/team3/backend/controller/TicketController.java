90361bd155cc1074eb77a9fef308a00fdf8cee2b
julia <julia.jodczyk@gmail.com>
2024-05-15 22:05:13 +0200

    APSI-25: send email to user

--- backend/src/main/java/apsi/team3/backend/controller/TicketController.java
+++ backend/src/main/java/apsi/team3/backend/controller/TicketController.java
@@ -4,6 +4,10 @@ import apsi.team3.backend.DTOs.TicketDTO;
 import apsi.team3.backend.exceptions.ApsiValidationException;
 import apsi.team3.backend.helpers.QRCodeGenerator;
 import apsi.team3.backend.interfaces.ITicketService;
+import apsi.team3.backend.model.MailStructure;
+import apsi.team3.backend.model.User;
+import apsi.team3.backend.services.MailService;
+import apsi.team3.backend.services.UserService;
 import com.google.zxing.WriterException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,8 +22,14 @@ import java.util.Optional;
 @CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
 public class TicketController {
     private final ITicketService ticketService;
+    private final MailService mailService;
+    private final UserService userService;
 
-    @Autowired TicketController(ITicketService ticketService) { this.ticketService = ticketService; }
+    @Autowired TicketController(ITicketService ticketService, MailService mailService, UserService userService) {
+        this.ticketService = ticketService;
+        this.mailService = mailService;
+        this.userService = userService;
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<TicketDTO> getTicketById(@PathVariable("id") Long id) {
@@ -27,10 +37,21 @@ public class TicketController {
         return ticket.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    // todo 3: display QR code inside mail message
+
     @PostMapping
     public ResponseEntity<TicketDTO> createTicketType(@RequestBody TicketDTO ticketDTO) throws ApsiValidationException, IOException, WriterException {
         var resp = ticketService.create(ticketDTO);
+        var user = userService.getUserById(ticketDTO.getHolderId());
         resp.setQRCode(QRCodeGenerator.generateQRCode(ticketDTO.toString()));
+
+        String mailSubject = "Your ticket is here!";
+        MailStructure mailStructure = new MailStructure(
+                mailSubject,
+                resp.getQRCode()
+        );
+        mailService.sendMail(user.get().getEmail(), mailStructure);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(resp);
     }
 }
--- backend/src/main/java/apsi/team3/backend/model/MailStructure.java
+++ backend/src/main/java/apsi/team3/backend/model/MailStructure.java
@@ -1,10 +1,12 @@
 package apsi.team3.backend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class MailStructure {
     private String subject;
     private String message;
