2bedb646c900e2ad9a2fbd24f8f4fcb463e682db
julia <julia.jodczyk@gmail.com>
2024-05-15 21:27:36 +0200

    APSI-25: fixed mail sending

--- backend/src/main/java/apsi/team3/backend/services/TicketService.java
+++ backend/src/main/java/apsi/team3/backend/services/TicketService.java
@@ -3,6 +3,7 @@ package apsi.team3.backend.services;
 import apsi.team3.backend.DTOs.DTOMapper;
 import apsi.team3.backend.DTOs.TicketDTO;
 import apsi.team3.backend.interfaces.ITicketService;
+import apsi.team3.backend.model.MailStructure;
 import apsi.team3.backend.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,10 @@ import java.util.Optional;
 @Service
 public class TicketService implements ITicketService {
     private final TicketRepository ticketRepository;
-    private final MailSenderService mailSenderService;
+    private final MailService mailSenderService;
 
     @Autowired
-    public TicketService(TicketRepository ticketRepository, MailSenderService mailSenderService) { this.ticketRepository = ticketRepository;
+    public TicketService(TicketRepository ticketRepository, MailService mailSenderService) { this.ticketRepository = ticketRepository;
         this.mailSenderService = mailSenderService;
     }
 
@@ -28,7 +29,7 @@ public class TicketService implements ITicketService {
     public TicketDTO create(TicketDTO ticketDTO) {
         var ticket = DTOMapper.toEntity(ticketDTO);
         var saved = ticketRepository.save(ticket);
-        mailSenderService.sendMailMessage("mietowka06@gmail.com", "Test subject", "Body body body");
+
         return DTOMapper.toDTO(saved);
     }
 }
