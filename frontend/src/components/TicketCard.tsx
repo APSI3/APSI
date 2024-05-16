a7b297f9fa06ac23ca71dc41acf41f2db89136c2
julia <julia.jodczyk@gmail.com>
2024-05-16 16:01:31 +0200

    APSI-25: improve styling

--- frontend/src/components/TicketCard.tsx
+++ frontend/src/components/TicketCard.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import { borders } from '@mui/system';
 import { TicketTypeDTO} from "../api/DTOs";
 import { Grid, Paper, Typography} from "@mui/material";
 import {Api} from "../api/Api";
@@ -18,19 +19,19 @@ const TicketCard: React.FC<{ ticket: TicketTypeDTO, skipApiCheck?: boolean }> = 
     }, [ticket.id, skipApiCheck]);
 
     return (
-        <Paper style={{ alignItems: 'center', display: 'flex', justifyContent: 'space-evenly' }}>
-            <Grid item xs={6}>
+        <Paper style={{ alignItems: 'center', display: 'flex', justifyContent: 'space-evenly', margin: '0.5rem', background: '#dee2e6' }}  elevation={3} >
+            <Grid item xs={6} >
                 <Typography variant="h6" component="h2" >
                     {ticket.name}
                 </Typography>
             </Grid>
-            <Grid item container direction="row">
-                <BuyButton ticketTypeId={ticket.id} />
+            <Grid item container direction="row" justifyContent="flex-end" padding='1rem' style={{background: '#ffffff'}}>
+                <BuyButton ticketTypeId={ticket.id}/>
                 <Grid item container direction="column" alignItems="flex-end">
-                    <Typography variant="body1" color="textSecondary" style={{marginRight: '1rem'}}>
+                    <Typography variant="body1" color="textSecondary" style={{ marginTop: '1rem' }}>
                         <strong>{ticket.price.toFixed(2)} zł</strong>
                     </Typography>
-                    <Typography variant="body2" color="textSecondary" style={{marginRight: '1rem'}}>
+                    <Typography variant="body2" color="textSecondary" >
                         Dostępność: {ticket.quantityAvailable - soldCount}/{ticket.quantityAvailable}
                     </Typography>
                 </Grid>
