b1d6499f4bebc2a995a67a1454ab2aaeec61f7cb
julia <julia.jodczyk@gmail.com>
2024-05-18 01:43:11 +0200

    APSI-25: read from resources

--- backend/src/main/java/apsi/team3/backend/helpers/MailGenerator.java
+++ backend/src/main/java/apsi/team3/backend/helpers/MailGenerator.java
@@ -1,5 +1,10 @@
 package apsi.team3.backend.helpers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.DateFormat;
@@ -7,14 +12,15 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MailGenerator {
 
-    private static final String templatePath = "backend/src/main/resources/templates/mailTemplate.html";
+    private static final String templatePath = "templates/mailTemplate.html";
 
     public static String generateTicket(Map<String, String> ticketData) {
         try {
-            String templateContent = new String(Files.readAllBytes(Paths.get(templatePath)));
+            String templateContent = readResourceFile(templatePath);
             for (Map.Entry<String, String> entry : ticketData.entrySet()) {
                 templateContent = templateContent.replace("$" + entry.getKey(), entry.getValue());
             }
@@ -30,5 +36,22 @@ public class MailGenerator {
         String strFromDate = fromDate.format(formatter);
         String strToDate = toDate.format(formatter);
         return String.format("%s - %s", strFromDate, strToDate);
+    }
+
+    public static String readResourceFile(String fileName) {
+        // Get the class loader
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        // Read the file using InputStream
+        try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
+            assert inputStream != null;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+
+                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
