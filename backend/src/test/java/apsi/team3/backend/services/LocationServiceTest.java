bc4e571ab26fa48238c613d95b8b97491c96580d
julia <julia.jodczyk@gmail.com>
2024-05-17 16:33:30 +0200

    APSI-25: ticket controller tests

--- backend/src/test/java/apsi/team3/backend/services/LocationServiceTest.java
+++ backend/src/test/java/apsi/team3/backend/services/LocationServiceTest.java
@@ -34,7 +34,7 @@ public class LocationServiceTest {
 
     @Test
     public void testGetLocationByIdReturnsLocation() {
-        User creator = new User(1L, "login", "hash", "salt", UserType.ORGANIZER, new ArrayList<>());
+        User creator = new User(1L, "login", "hash", "salt", UserType.ORGANIZER, "email", new ArrayList<>());
         Location location = new Location(
             1L,
             creator,
@@ -68,7 +68,7 @@ public class LocationServiceTest {
         Location location = DTOMapper.toEntity(locationDTO);
         location.setId(1L);
         try (var securityContextHolderMockedStatic = mockStatic(SecurityContextHolder.class)) {
-            User creator = new User(1L, "login", "hash", "salt", UserType.ORGANIZER, new ArrayList<>());
+            User creator = new User(1L, "login", "hash", "salt", UserType.ORGANIZER, "email", new ArrayList<>());
             SecurityContext securityContextMock = mock(SecurityContext.class);
             securityContextHolderMockedStatic.when(SecurityContextHolder::getContext).thenReturn(securityContextMock);
             Authentication authenticationMock = mock(Authentication.class);
