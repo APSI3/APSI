81959946cc02b74a38d5e1149ca4cbeddd13c624
julia <julia.jodczyk@gmail.com>
2024-06-12 20:16:13 +0200

    APSI-63: fetch new endpoint on frontend

--- frontend/src/pages/LocationsPage.tsx
+++ frontend/src/pages/LocationsPage.tsx
@@ -1,5 +1,30 @@
+import {useCallback, useEffect, useState} from "react";
+import {LocationDTO} from "../api/DTOs";
+import {Api} from "../api/Api";
+
 export default function LocationsPage() {
-    // todo: [JIRA: APSI-28]
+    const [locations, setLocations] = useState<LocationDTO[]>([]);
+    const [currentIdx, setCurrentIdx] = useState(0);
+    const [maxIdx, setMaxIdx] = useState(0);
+
+    const getLocations = useCallback((currentIdx: number) => {
+        Api.GetLocationsPageable(currentIdx).then(res => {
+            if (res.success && res.data) {
+                console.log(res.data.items);
+                setLocations(res.data.items ?? []);
+                setMaxIdx(res.data.totalPages - 1);
+            }
+        })
+    }, []);
+
+    useEffect(() => {
+        getLocations(currentIdx);
+    }, [currentIdx]);
+
+    const handlePageChange = (index: number) => {
+        setCurrentIdx(index);
+        getLocations(index);
+    }
     return <>
         Work in progress
     </>
